f9c037d2c876990594563a485cb2d98354d3fa2f
Abiud Sokomi <abiudsokomi@gmail.com>
2023-11-21 09:57:20 +0300

    my commit

---
+++ script.js
@@ -0,0 +1,22 @@
+//code using while loop
+
+/*var i = 0;
+
+while (i<10) {
+    document.write(i + "<br/>");
+    i++;
+}
+*/
+
+/*code using for loop
+
+for (let i = 0; i < 10; i++) {
+    document.write(i + "<br/>");   
+}*/
+
+//Using for loops to print out elements in an array
+
+let friends = ["Abiud", "Stanley", "Noel"]
+for (let i = 0; i < friends.length; i++) {
+    document.write(friends[i] + "<br/>");   
+}
